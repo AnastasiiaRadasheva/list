66eb3a03d46cb05f7ee49f7130023d958e8b2931
opilane <opilane@HP-CZC2349HTC>
2025-03-10 09:51:53 +0200

    bebe

---
+++ work.py
@@ -0,0 +1,9 @@
+﻿
+# Объединение слов в строку с разными разделителями
+joined_comma = ", ".join(words)
+joined_semicolon = "; ".join(words)
+joined_dash = " - ".join(words)
+print("Строка с запятой:", joined_comma)
+print("Строка с точкой с запятой:", joined_semicolon)
+print("Строка с тире:", joined_dash)
+
